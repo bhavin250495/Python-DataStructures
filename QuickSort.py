79259f4adfdaeb5859e304d2c49eabd791dac437
Bhavin Suthar <bhavinathunt@gmail.com>
2019-02-05 11:39:46 +0530

    Added comments

--- QuickSort.py
+++ QuickSort.py
@@ -1,21 +1,31 @@
 
 def quick_sort(arr):
 
-    # Check if array is divisable
+    ''' Check if array is divisable else 
+    retrun single element array '''
     if len(arr)>1:
         
         start_index = 0
         end_index = len(arr)
-        mid_point = int((start_index+end_index)/2)
+        mid_index = int((start_index+end_index)/2)
 
-        list1 = list(filter(lambda x : x < arr[mid_point],arr))
-        list2 = list(filter(lambda x : x > arr[mid_point],arr))
+        '''
+        Divide the array in 2 sub arrays and arrange them as  
+        1 - This will put the mid point in its sorted index
+        2 - Smaller then mid_point 
+        3 - Greater then mid_point
+        '''
+        list1 = list(filter(lambda x : x < arr[mid_index],arr))
+        list2 = list(filter(lambda x : x > arr[mid_index],arr))
 
+        '''Continue division till we get last undivisable element'''
         list1 = quick_sort(list1)
+        
         list2 = quick_sort(list2)
 
-        list1.append(arr[mid_point])
+        list1.append(arr[mid_index])
 
+        '''Append sorted sub arrays and return '''
         return(list1+list2)   
     else:
         return arr      
